0deb1474b6f7735244790993fbb7521dc283ed65
Michael Sanchez <Michael.s140701@gmail.com>
2025-08-01 21:13:16 -0400

    error sin querer

--- libreria/views.py
+++ libreria/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def inicio(request):
+    return HttpResponse("<h1>Bienvenido</h1>")
+
+def nosotros(request):
+    return render(request, 'paginas/nosotros.html')
+
+def usuarios(request):
+    return render(request, 'usuarios/index.html')
